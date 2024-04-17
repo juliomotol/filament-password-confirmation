71a068c8bab902da22f3d81cebf1f532eca8cc16
Julio Motol <julio.motol89@gmail.com>
2024-04-17 20:01:17 +0800

    ✨ Add configurability
    Signed-off-by: Julio Motol <julio.motol89@gmail.com>

--- src/FilamentPasswordConfirmationPlugin.php
+++ src/FilamentPasswordConfirmationPlugin.php
@@ -9,6 +9,12 @@ use JulioMotol\FilamentPasswordConfirmation\Pages\ConfirmPassword;
 
 class FilamentPasswordConfirmationPlugin implements Plugin
 {
+    protected string $routeName = 'confirm';
+
+    protected string $routeUri = 'auth/confirm';
+
+    protected string | array | null $routeMiddleware = null;
+
     public function getId(): string
     {
         return 'filament-password-confirmation';
@@ -16,8 +22,10 @@ class FilamentPasswordConfirmationPlugin implements Plugin
 
     public function register(Panel $panel): void
     {
-        // TODO Configure name, path, middleware
-        $panel->authenticatedRoutes(fn () => Route::get('confirm', ConfirmPassword::class)->name('confirm'));
+        $panel->authenticatedRoutes(
+            fn () => Route::middleware($this->routeMiddleware)->get($this->routeUri, ConfirmPassword::class)
+                ->name($this->routeName)
+        );
 
         $panel->macro('getPasswordConfirmationRouteName', fn () => $panel->generateRouteName('confirm'));
     }
@@ -38,5 +46,26 @@ class FilamentPasswordConfirmationPlugin implements Plugin
         $plugin = filament(app(static::class)->getId());
 
         return $plugin;
+    }
+
+    public function routeName(string $routeName): self
+    {
+        $this->routeName = $routeName;
+
+        return $this;
+    }
+
+    public function routeUri(string $routeUri): self
+    {
+        $this->routeUri = $routeUri;
+
+        return $this;
+    }
+
+    public function routeMiddleware(string | array | null $routeMiddleware): self
+    {
+        $this->routeMiddleware = $routeMiddleware;
+
+        return $this;
     }
 }
--- src/RequiresPasswordConfirmation.php
+++ src/RequiresPasswordConfirmation.php
@@ -9,10 +9,12 @@ use Illuminate\Support\Arr;
 /** @property-read string|array $routeMiddleware */
 trait RequiresPasswordConfirmation
 {
+    protected static ?int $passwordTimeout = null;
+
     public static function getRouteMiddleware(Panel $panel): string | array
     {
         return [
-            RequirePassword::using($panel->getPasswordConfirmationRouteName()),
+            RequirePassword::using($panel->getPasswordConfirmationRouteName(), static::$passwordTimeout),
             ...Arr::wrap(static::$routeMiddleware),
         ];
     }
